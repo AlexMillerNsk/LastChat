1aae2b8818d49f2533147e4fa7bcdbf4b2e1bc7e
Aleksandr <love.me.lnikov@gmail.com>
2022-10-26 22:31:17 +0700

    first commit

---
+++ Client/Program.fs
@@ -0,0 +1,50 @@
+﻿module Program
+
+open Browser
+open Elmish
+open Elmish.React
+open Fable.Core.JS
+open Fable.React
+open Props
+
+type Model = { State: string
+               TextBox: string
+               Content: string  }
+
+type Msg =
+    | SentRequest
+    | SetTextBox of string
+    | SetResponse of string
+    | SetChat of string
+
+let webSocket = WebSocket.Create("ws://127.0.0.1:8080/websocket")
+let registerOnMessageHandler =
+    fun dispatch ->
+        async {
+            webSocket.addEventListener_message (fun xy -> dispatch <| SetChat (xy.data.ToString()))
+        } |> Async.StartImmediate
+
+let init() = { State = "test"
+               TextBox = ""
+               Content = "jjhhjj" }, [ registerOnMessageHandler ]
+let sendRequest model = fun _ -> async { webSocket.send($"{model.TextBox}") } |> Async.StartImmediate
+    
+let update msg model =
+    match msg with
+    | SentRequest    -> model, [ sendRequest model ]
+    | SetTextBox v   -> { model with TextBox = v }, Cmd.none
+    | SetResponse r  -> { model with State = r }, Cmd.none
+    | SetChat x      -> { model with Content = x }, Cmd.none
+
+
+let view (model: Model) dispatch =
+    div [] [
+        div [] [ str model.State ]
+        button [ OnClick (fun _ -> dispatch SentRequest) ] [ str "SendRequest" ]
+        div [  ] [ input [ Value model.TextBox; OnChange (fun e -> dispatch (SetTextBox e.Value) ) ] ]
+        str (model.Content.ToString()) ]
+
+Program.mkProgram init update view
+//|> Program.withSubscription onMessage 
+|> Program.withReactSynchronous "elmish-app"
+|> Program.run
---
+++ Server/Program.fs
@@ -0,0 +1,66 @@
+﻿module Server
+
+open System.IO
+open Suave
+open Suave.Http
+open Suave.Sockets.Control
+open Suave.Utils
+open Filters
+open Operators
+open WebSocket
+open Suave.Sockets
+
+type State = {Subscribers:WebSocket list}
+
+type Msg =
+    | SendAll of ByteSegment
+    | Subscribe of WebSocket
+
+let processor = MailboxProcessor<Msg>.Start(fun inbox ->
+    let rec innerLoop state  = async {
+        let! message = inbox.Receive()
+        match message with
+        | SendAll msg -> 
+//            state.Subscribers|>List.iter (fun x ->  x.send Text msg true)
+            for x in state.Subscribers do
+                let! result = x.send Text msg true
+                ()
+            do! innerLoop state
+        | Subscribe ws ->
+            let state = { state with Subscribers = ws::state.Subscribers } 
+            do! innerLoop state
+        ()
+         }
+    innerLoop {Subscribers=[]})
+
+let ws (webSocket : WebSocket) _ =
+    processor.Post(Subscribe webSocket)
+    socket {
+        let mutable loop = true
+        
+        while loop do
+            let! _, input, _ = webSocket.read()
+            let text = ASCII.toString input
+            let byteResponse =
+                text
+                |> System.Text.Encoding.ASCII.GetBytes
+                |> ByteSegment
+//            do! webSocket.send Text byteResponse true
+            printfn $"{byteResponse}"
+            processor.Post(SendAll byteResponse )
+         }
+let app: WebPart =
+     choose [
+          GET >=> path "/" >=> Files.file "./public/index.html"    
+          GET >=> path "/test" >=> Successful.OK "Biden666"
+          GET >=> Files.browseHome
+          path "/websocket" >=> handShake ws
+          RequestErrors.NOT_FOUND "Page not found." ]
+
+let config = {
+    defaultConfig with
+        bindings = [HttpBinding.createSimple Protocol.HTTP "127.0.0.1" 8080]
+        homeFolder = Some(Path.GetFullPath "./public")
+}
+
+startWebServer config app
