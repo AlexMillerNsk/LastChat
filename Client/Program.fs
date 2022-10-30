d7f46e78c599d5e9eb44951280c73af1d64bc960
Aleksandr <love.me.lnikov@gmail.com>
2022-10-30 22:12:04 +0700

    style added

--- Client/Program.fs
+++ Client/Program.fs
@@ -3,11 +3,7 @@
 open Browser
 open Elmish
 open Elmish.React
-open Fable.Core.JS
-open Fable.React
-open Props
-open System.Net.Sockets
-
+open Feliz
 
 type Page =
     |Autorisation
@@ -29,7 +25,7 @@ type Msg =
 
 
 
-let webSocket = WebSocket.Create($"ws://192.168.68.104:8080/websocket")
+let webSocket = WebSocket.Create($"ws://192.168.0.170:8080/websocket")
 let registerOnMessageHandler =
     fun dispatch ->
         async {
@@ -48,26 +44,88 @@ let update msg model =
     | SentRequest    -> model, [ sendRequest model ]
     | SetTextBox v   -> { model with TextBox = v }, Cmd.none
     | SetResponse r  -> { model with State = r }, Cmd.none
-    | SetChat x      -> { model with Content = x::model.Content}, Cmd.none
+    | SetChat x      -> { model with
+                            TextBox = ""
+                            Content = x::model.Content}, Cmd.none
     | Autorisation page -> {model with CurrentPage = page}, Cmd.none
     | SetName x      -> {model with Name = x}, Cmd.none
 
+let appTitle =
+  Html.p [
+    prop.className "title"
+    prop.text "Welcome to our Awesome Chat!"
+  ]
 
+let chatList (model: Model) (dispatch: Msg -> unit) =
+  Html.ul [
+    prop.children [
+      for chat in model.Content ->
+        Html.li [
+          prop.classes ["box"; "subtitle"]
+          prop.text chat
+        ]
+    ]
+  ]
+
+let inputField (model: Model) (dispatch: Msg -> unit) =
+  Html.div [
+    prop.classes [ "field"; "has-addons" ]
+    prop.children [
+      Html.div [
+        prop.classes [ "control"; "is-expanded"]
+        prop.children [
+          Html.input [
+            prop.classes [ "input"; "is-medium" ]
+            prop.valueOrDefault model.TextBox
+            prop.onChange (SetTextBox >> dispatch)
+          ]
+        ]
+      ]
+      Html.div [
+        prop.className "control"
+        prop.children [
+          Html.button [
+            prop.classes [ "button"; "is-primary"; "is-medium" ]
+            prop.onClick (fun _ -> dispatch SentRequest)
+            prop.children [
+              Html.i [ prop.classes [ "fa"; "fa-plus" ] ]
+            ]
+          ]
+        ]
+      ]
+    ]
+  ]
 let view (model: Model) dispatch =
     match model.CurrentPage with
     | Page.Chat ->
-        div [] [
-            div [] [ str model.State ]
-            button [ OnClick (fun _ -> dispatch SentRequest) ] [ str "SendRequest" ]
-            div [  ] [ input [ Value model.TextBox; OnChange (fun e -> dispatch (SetTextBox e.Value) ) ] ]
-            str (model.Content.ToString()) ]
-    | Page.Autorisation ->
-            div [] [
-            button [ OnClick (fun _ -> dispatch (Autorisation Page.Chat)) ] [ str "autorise" ]
-            div [  ] [ input [ Value model.Name; OnChange (fun e -> dispatch (SetName e.Value) ) ] ]
-            str (model.Name.ToString()) ]
+        //div [] [
+        //    div [] [ str model.State ]
+        //    button [ OnClick (fun _ -> dispatch SentRequest) ] [ str "SendRequest" ]
+        //    div [  ] [ input [ Value model.TextBox; OnChange (fun e -> dispatch (SetTextBox e.Value) ) ] ]
+        //    str (model.Content.ToString())]
+        Html.div [
+          prop.style [style.padding 20]
+          prop.children [
+            appTitle
+            inputField model dispatch         
+            chatList model dispatch          
+          ]     
+        ]
 
-
+            
+    | Page.Autorisation ->            
+            Html.div [
+              prop.style [style.padding 20]
+              prop.children [
+                  appTitle
+                  Html.button [
+                    prop.onClick  (fun _ -> dispatch (Autorisation Page.Chat)) 
+                    prop.text "Autorise"
+                    ]
+                  Html.input [ prop.onChange (SetName >> dispatch) ]
+                  Html.span $"Your name will be {model.Name}"
+               ]
+            ]
 Program.mkProgram init update view
 //|> Program.withSubscription onMessage 
 |> Program.withReactSynchronous "elmish-app"
